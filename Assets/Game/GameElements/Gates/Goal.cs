f7334bd907ce2606a629d4dc338a7c9a5916feac
moisesjpelaez <moises.jpelaez@gmail.com>
2021-11-15 17:30:33 -0400

    Switch and goal added. Gitignore rules added.

---
+++ Assets/Game/GameElements/Gates/Goal.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public void GoalReached()
+    {
+        Debug.Log("Level Win!");
+    }
+}
