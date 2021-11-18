1f9266798a5fea5cd65139fd72ba60b07d62c9e4
Marco Marmolejos <marco.marmolejos@gmail.com>
2021-11-18 11:56:12 -0400

    Added reset level

--- Assets/Game/GameElements/LevelCommon/GameplayController.cs
+++ Assets/Game/GameElements/LevelCommon/GameplayController.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using JohannesMP;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using static UnityEngine.InputSystem.InputAction;
 
 public class GameplayController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private InputActionAsset inputActionAsset;
+    [SerializeField] private SceneTransitionerChannel sceneTransition;
+    //[SerializeField] private SceneReference menuScene;
+
+    private InputActionMap actionMap;
+    private InputAction reset;
+
     void Start()
     {
-        
+        actionMap = inputActionAsset.FindActionMap("Gameplay");
+        reset = actionMap.FindAction("Reset");
+
+        reset.Enable();
+
+        reset.performed += OnReset;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        reset.performed += OnReset;
     }
+
+    private void OnReset(CallbackContext ctx)
+    {
+        // reset game
+        sceneTransition.LoadScene(SceneManager.GetActiveScene().path);
+    }
+
 }
