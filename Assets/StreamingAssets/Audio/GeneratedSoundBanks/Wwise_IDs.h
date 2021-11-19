5259c1a9fd3f1d6eb3202eef8da6ef69dd693a66
Ismael Ortiz Audio <contact@ismaelortiz.com>
2021-11-19 21:10:06 +0000

    - Added soundbanks to Unity

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,24 @@ namespace AK
 {
     namespace BUSSES
     {
+        static const AkUniqueID _2D = 527871411U;
+        static const AkUniqueID _3D = 511093792U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID HELICOPTER = 2167849898U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MECHA = 3125191939U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID TANK = 3206747537U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID VEHICLES = 971393558U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID VERB = 3657263530U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
